dff03e1dfc345fa096aa49425f284ae3bbcb5574
Ketlin Oliveira <ketlinkii2004@gmail.com>
2026-04-02 21:03:32 -0300

    atualiza versão do SendGrid

--- src/main/java/ketlin/portifolio/service/EmailService.java
+++ src/main/java/ketlin/portifolio/service/EmailService.java
@@ -8,6 +8,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ketlin.portifolio.dto.ContatoDTO;
 import java.io.IOException;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 @Service
 public class EmailService {
