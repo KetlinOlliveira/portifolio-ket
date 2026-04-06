70d0256f8167cfd5972a7cc9a391444afff0a7af
Ketlin Oliveira <ketlinkii2004@gmail.com>
2026-04-05 22:56:20 -0300

    att: atualiza a pasta vite.config.js

--- frontendd/vite.config.js
+++ frontendd/vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Isso força o Vite a usar caminhos relativos
   server: {
     proxy: {
-      // Isso resolve o problema de CORS no desenvolvimento!
-      // Toda chamada para /api vai ser redirecionada ao seu backend Java
       '/api': {
         target: 'https://portifolio-ket-production.up.railway.app',
         changeOrigin: true,
